a316e26874f505c5a322d2aa2ec6e2620c58b6a1
Thadeu Castelo Branco Ramos <thadeucbr@gmail.com>
2025-08-10 15:22:00 +0000

    feat: remove deprecated processMessageAI and processMessageAI_legacy files

--- src/skills/processMessageAI_legacy.js
+++
@@ -1,7 +0,0 @@
-// DEPRECATED: Este arquivo foi refatorado e movido para /src/core/
-// Mantendo apenas para compatibilidade com imports existentes
-// TODO: Atualizar todos os imports para usar o novo messageProcessor
-
-import processMessage from '../core/messageProcessor.js';
-
-export default processMessage;
