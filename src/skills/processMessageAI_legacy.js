dcf42d5d2511e951ffa565c6c4741fcc50581792
Thadeu Castelo Branco Ramos <thadeucbr@gmail.com>
2025-08-10 15:09:10 +0000

    feat: Adiciona documentação técnica e refatoração do core da aplicação para arquitetura modular

---
+++ src/skills/processMessageAI_legacy.js
@@ -0,0 +1,7 @@
+// DEPRECATED: Este arquivo foi refatorado e movido para /src/core/
+// Mantendo apenas para compatibilidade com imports existentes
+// TODO: Atualizar todos os imports para usar o novo messageProcessor
+
+import processMessage from '../core/messageProcessor.js';
+
+export default processMessage;
